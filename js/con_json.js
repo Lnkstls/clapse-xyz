449302977c1eef0490da9416dad8528bdb73d7f4
Lnkstls <lnkstls@gmail.com>
2020-04-16 23:26:43 +0800

    更加灵活更改订阅展示

--- js/con_json.js
+++ js/con_json.js
@@ -1,8 +1,9 @@
 var con_json = {
     "jlist":[
-        {"jianame":"月付套餐","mon":9.9,"wik":"<li><p><strong>80</strong> GB流量</p></li><li><p><strong>无连接设备限制</strong> </p></li><li><p>(禁止倒卖、恶意分享，一经发现将永久封禁)</p></li>"},
-        {"jianame":"季付套餐","mon":29.9,"wik":"<li><p><strong>85</strong> GB流量</p></li><li><p><strong>无连接设备限制</strong> </p></li><li><p>(禁止倒卖、恶意分享，一经发现将永久封禁)</p></li>"},
-        {"jianame":"年付套餐","mon":99.9,"wik":"<li><p><strong>100</strong> GB流量</p></li><li><p><strong>无连接设备限制</strong> </p></li><li><p>(禁止倒卖、恶意分享，一经发现将永久封禁)</p></li>"},
+        {"jianame":"VIP2","back":"rgba(0, 0, 0, .7)","mon":9.9,"wik":"<li><p><strong>150</strong> GB流量</p></li><li><p><strong>无连接设备限制</strong> </p></li><li><p>(禁止倒卖、恶意分享，一经发现将永久封禁)</p></li>"},
+        // {"jianame":"季付套餐","back":"rgba(0,150,136,.7)","mon":29.9,"wik":"<li><p><strong>85</strong> GB流量</p></li><li><p><strong>无连接设备限制</strong> </p></li><li><p>(禁止倒卖、恶意分享，一经发现将永久封禁)</p></li>"},
+        // {"jianame":"年付套餐","back":"rgba(244,67,54,.7)","mon":99.9,"wik":"<li><p><strong>100</strong> GB流量</p></li><li><p><strong>无连接设备限制</strong> </p></li><li><p>(禁止倒卖、恶意分享，一经发现将永久封禁)</p></li>"},
+        // {"jianame":"年付套餐","back":"rgba(244,67,54,.7)","mon":99.9,"wik":"<li><p><strong>100</strong> GB流量</p></li><li><p><strong>无连接设备限制</strong> </p></li><li><p>(禁止倒卖、恶意分享，一经发现将永久封禁)</p></li>"},  
     ],
     "phone":"<a target='_blank' href='https://t.me/clapsev' class='bottom-lx'>TG群组</a>、<a target='_blank' href='https://t.me/clapserss' class='bottom-lx'>TG通知频道</a>、<a target='_blank' href='https://tg.clapse.xyz/s/clapserss' class='bottom-lx'>应急通知</a>"
 };
--- js/domjs.js
+++ js/domjs.js
@@ -1,13 +1,26 @@
 // 价格列表
-for (let i = 0; i < con_json.jlist.length; i++) {
-    let name = con_json.jlist[i].jianame;
-    let mon = con_json.jlist[i].mon;
-    let wik = con_json.jlist[i].wik;
-    $("#jianame"+i).append(name);
-    $("#mon"+i).append(mon);
-    $("#wik"+i).append(wik);
+if (con_json.jlist.length > 0) {
+	if (con_json.jlist.length > 0) {
+		for (let i = 1; i < con_json.jlist.length; i++) {
+			$("#jlistdom").append($("#jdom").clone(true));
+		}
+	}
+	for (let i = 0; i < con_json.jlist.length; i++) {
+		let name = con_json.jlist[i].jianame;
+		let mon = con_json.jlist[i].mon;
+		let wik = con_json.jlist[i].wik;
+		let back = con_json.jlist[i].back;
+		document.querySelectorAll("#jdom>div>div")[
+			i
+		].style.backgroundColor = back;
+		document.querySelectorAll("#jianame")[i].append(name);
+		document.querySelectorAll("#mon")[i].append(mon);
+		document.querySelectorAll("#wik")[i].innerHTML = wik;
+		console.log(i);
+	}
+} else {
+	$("#but_dyue").remove();
 }
 
 // 联系我们
 $("#lxwm").append(con_json.phone);
-
